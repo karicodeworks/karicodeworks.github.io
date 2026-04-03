13900baf71b0fa82d3d37763c643904ef756205d
James <nyamuj59@gmail.com>
2026-04-03 11:04:02 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import { title } from 'process'
+
+const projects = [
+  {
+    title: '',
+    description: '',
+    tech: ['', ''],
+    live: '#',
+    github: '#',
+  },
+  {
+    title: '',
+    description: '',
+    tech: ['', ''],
+    live: '#',
+    github: '#',
+  },
+]
+
+export default function Home() {
+  return (
+    <main className='bg-[#0B0F19] text-gray-200 min-h-screen px-6'>
+      <div className='max-w-5xl mx-auto'></div>
+    </main>
+  )
+}
